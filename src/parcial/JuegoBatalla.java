db5878407f058de5d0ca0340a820bd03f7b598a3
estudiante311 <estudiante311@DESKTOP-KHLV98F.unicamacho.co>
2025-04-08 21:13:19 -0500

    Batalla magistral de pokemones

--- src/parcial/JuegoBatalla.java
+++ src/parcial/JuegoBatalla.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package criatura2;
+package parcial;
 
 public class JuegoBatalla {
     public static void main(String[] args) {
